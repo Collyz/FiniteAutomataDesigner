6c35b8316b6ae05406a826b20f12e77518004e0f
ParthP22 <parthpatel0422@gmail.com>
2026-07-23 16:01:38 -0400

    feat: updated automataApi following the recent changes to the Window interface

--- finite-automata-designer/src/app/components/editor/api/automataApi.ts
+++ finite-automata-designer/src/app/components/editor/api/automataApi.ts
@@ -2,11 +2,13 @@ import { SerializedFA } from "@/lib/shared/types";
 
 export const automataApi = {
     DFSM: {
-        loadFAIntoCanvas: (data: SerializedFA) => window.loadDFSMIntoCanvas(data),
+        loadFAIntoCanvas: (projectId: string | null, data: SerializedFA) => window.loadDFSMIntoCanvas(projectId, data),
         exportFA: () => window.exportDFSM(),
+        getCurrentProjectId: () => window.getCurrentDFSMProjectId(),
     },
     NDFSM: {
-        loadFAIntoCanvas: (data: SerializedFA) => window.loadNDFSMIntoCanvas(data),
+        loadFAIntoCanvas: (projectId: string | null, data: SerializedFA) => window.loadNDFSMIntoCanvas(projectId, data),
         exportFA: () => window.exportNDFSM(),
+        getCurrentProjectId: () => window.getCurrentNDFSMProjectId(),
     },
 } as const;
