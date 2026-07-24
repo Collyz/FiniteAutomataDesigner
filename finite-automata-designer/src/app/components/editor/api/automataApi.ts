be1074e71e9a4490d8c0dfd5ebea12ec6cee04b2
ParthP22 <parthpatel0422@gmail.com>
2026-07-23 20:16:13 -0400

    fix: the exposed clearCanvas function might not be initialized when rendering the canvas, so the code to check if it has been initialized has been moved into the automataApi itself

--- finite-automata-designer/src/app/components/editor/api/automataApi.ts
+++ finite-automata-designer/src/app/components/editor/api/automataApi.ts
@@ -4,11 +4,19 @@ export const automataApi = {
     DFSM: {
         loadFAIntoCanvas: (data: SerializedFA) => window.loadDFSMIntoCanvas(data),
         exportFA: () => window.exportDFSM(),
-        clearCanvas: () => window.clearDFSMCanvas(),
+        clearCanvas: () => {
+            if (typeof window.clearDFSMCanvas === "function") {
+                window.clearDFSMCanvas();
+            }
+        },
     },
     NDFSM: {
         loadFAIntoCanvas: (data: SerializedFA) => window.loadNDFSMIntoCanvas(data),
         exportFA: () => window.exportNDFSM(),
-        clearCanvas: () => window.clearNDFSMCanvas(),
+        clearCanvas: () => {
+            if (typeof window.clearNDFSMCanvas === "function") {
+                window.clearDFSMCanvas();
+            }
+        },
     },
 } as const;
